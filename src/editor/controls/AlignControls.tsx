9460a862e9baa76793eee45d735cded957543766
Taha Anılcan Metinyurt <igoodie@programmer.net>
2022-04-12 01:09:05 +0300

    feat: finish up with Align Controls

--- src/editor/controls/AlignControls.tsx
+++ src/editor/controls/AlignControls.tsx
@@ -23,7 +23,7 @@ export const AlignControls = (props: Props) => {
     }
     return props.layers.selectedLayers.reduce(
       (minValue, layer) => Math.min(minValue, layer.x),
-      props.layers.selectedLayers[0].x
+      Number.MAX_SAFE_INTEGER
     );
   };
 
@@ -35,7 +35,18 @@ export const AlignControls = (props: Props) => {
       const width = ctx.layers.getWidth(layer, ctx);
       const widthMm = ctx.paperUnit.toMillimeters(width);
       return Math.max(maxValue, layer.x + widthMm);
-    }, 0);
+    }, Number.MIN_SAFE_INTEGER);
+  };
+
+  const getCenterX = () => {
+    if (props.layers.selectedLayers.length == 1) {
+      return ctx.paperUnit.toMillimeters(
+        ctx.editorProps.paperDimensions.width / 2
+      );
+    }
+    const minX = getMinX();
+    const maxX = getMaxX();
+    return (maxX + minX) / 2;
   };
 
   const getMinY = () => {
@@ -44,7 +55,7 @@ export const AlignControls = (props: Props) => {
     }
     return props.layers.selectedLayers.reduce(
       (minValue, layer) => Math.min(minValue, layer.y),
-      props.layers.selectedLayers[0].y
+      Number.MAX_SAFE_INTEGER
     );
   };
 
@@ -58,12 +69,33 @@ export const AlignControls = (props: Props) => {
       const height = ctx.layers.getHeight(layer, ctx);
       const heightMm = ctx.paperUnit.toMillimeters(height);
       return Math.max(maxValue, layer.y + heightMm);
-    }, 0);
+    }, Number.MIN_SAFE_INTEGER);
+  };
+
+  const getCenterY = () => {
+    if (props.layers.selectedLayers.length == 1) {
+      return ctx.paperUnit.toMillimeters(
+        ctx.editorProps.paperDimensions.height / 2
+      );
+    }
+    const minY = getMinY();
+    const maxY = getMaxY();
+    return (maxY + minY) / 2;
   };
 
   const alignLeft = () => {
     const minX = getMinX();
     props.layers.selectedLayers.forEach((layer) => (layer.x = minX));
+    props.layers.updateLayers();
+  };
+
+  const alignCenterH = () => {
+    const centerX = getCenterX();
+    props.layers.selectedLayers.forEach((layer) => {
+      const width = ctx.layers.getWidth(layer, ctx);
+      const widthMm = ctx.paperUnit.toMillimeters(width);
+      layer.x = centerX - widthMm / 2;
+    });
     props.layers.updateLayers();
   };
 
@@ -93,17 +125,33 @@ export const AlignControls = (props: Props) => {
     props.layers.updateLayers();
   };
 
+  const alignCenterV = () => {
+    const centerY = getCenterY();
+    props.layers.selectedLayers.forEach((layer) => {
+      const height = ctx.layers.getHeight(layer, ctx);
+      const heightMm = ctx.paperUnit.toMillimeters(height);
+      layer.y = centerY - heightMm / 2;
+    });
+    props.layers.updateLayers();
+  };
+
   return (
     <ButtonStripeControl.Stripe>
       <ButtonStripeControl.Button icon={<AlignLeft />} onClick={alignLeft} />
 
-      <ButtonStripeControl.Button icon={<AlignCenterH />} />
+      <ButtonStripeControl.Button
+        icon={<AlignCenterH />}
+        onClick={alignCenterH}
+      />
 
       <ButtonStripeControl.Button icon={<AlignRight />} onClick={alignRight} />
 
       <ButtonStripeControl.Button icon={<AlignTop />} onClick={alignTop} />
 
-      <ButtonStripeControl.Button icon={<AlignCenterV />} />
+      <ButtonStripeControl.Button
+        icon={<AlignCenterV />}
+        onClick={alignCenterV}
+      />
 
       <ButtonStripeControl.Button
         icon={<AlignBottom />}
